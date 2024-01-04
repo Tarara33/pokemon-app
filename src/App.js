51bacde06a74b81e332de7e96078a35dac7af68a
Sarina <sarina0618@gmail.com>
2024-01-04 15:51:27 +0900

    ✨ :色々やった

--- src/App.js
+++ src/App.js
@@ -1,60 +1,84 @@
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import PokemonThumbnails from './component/PokemonThumbnails';
 
 function App() {
-  const pokemon = [
+  const [allPokemons, setAllPokemons] = useState([]);
+
+  // 仮でデータを作成する
+  const pokemons = [
     {
-    id: 1,
-    name: "フシギダネ",
-    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    type: "くさ"
-  },
-  {
-    id: 2,
-    name: "フシギソウ",
-    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png",
-    type: "くさ"
-  },
-  {
-    id: 3,
-    name: "フシギバナ",
-    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png",
-    type: "くさ"
-  },
-]
+      id: 1,
+      name: "フシギダネ",
+      image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      type: "くさ"
+    },
+    {
+      id: 2,
+      name: "フシギソウ",
+      image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png",
+      type: "くさ"
+    },
+    {
+      id: 3,
+      name: "フシギバナ",
+      image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png",
+      type: "くさ"
+    },
+  ];
 
-const url = "https://pokeapi.co/api/v2/pokemon";
+  // APIからデータを取得する
+  // パラメータにlimitを設定し、20件取得する
+  const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon?limit=20");
 
-useEffect(() => {
-  fetch(url)
+  // 仮でフシギダネのURLを使用する
+  const pokemonUrl = "https://pokeapi.co/api/v2/pokemon/bulbasaur";
+
+  const getAllPokemons = () => {
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data.results);
+        setAllPokemons(data.results);
+        // 次の20件をURLにセットする
+        setUrl(data.next);
+      });
+  };
+
+  // 個別のポケモンのデータを１つ１つ取得
+  const createPokemonObject = ()  => {
+    fetch(pokemonUrl)
     .then(res => res.json())
     .then(data => {
       console.log(data);
-    })
-}, [])
+      // ポケモンの画像の場所
+      // - (ハイフン)にlintで自動で半角スペースが入ってしまうため、[]で対応
+      // data.sprites.other.official-artwork.front_default でも大丈夫です
+      console.log(data.sprites.other["official-artwork"].front_default);
+      // ポケモンのタイプの場所
+      console.log(data.types[0].type.name);
+    });
+  };
+
+  useEffect(() => {
+    getAllPokemons();
+    createPokemonObject();
+    }, [])
 
   return (
     <div className="app-container">
       <h1>ポケモン図鑑</h1>
       <div className='pokemon-container'>
         <div className='all-container'>
-        {/* 仮で３つの子コンポーネントを表示する */}
-          <PokemonThumbnails
-            id={pokemon[0].id}
-            name={pokemon[0].name}
-            image={pokemon[0].image}
-            type={pokemon[0].type} />
-          <PokemonThumbnails
-            id={pokemon[1].id}
-            name={pokemon[1].name}
-            image={pokemon[1].image}
-            type={pokemon[1].type} />
-          <PokemonThumbnails
-            id={pokemon[2].id}
-            name={pokemon[2].name}
-            image={pokemon[2].image}
-            type={pokemon[2].type} />
+          {pokemons.map((pokemon, index) => (
+            <PokemonThumbnails
+              id={pokemon.id}
+              name={allPokemons[index]?.name}
+              image={pokemon.image}
+              type={pokemon.type}
+              key={index}
+              />
+          ))}
         </div>
       </div>
     </div>
